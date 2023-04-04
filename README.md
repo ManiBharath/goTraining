a29d49b86cc620707c4fcb8ec0e919b3451b92fb
Mani <manibharath34@gmail.com>
2023-04-05 02:31:25 +0530

    first end point

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # goTraining
 Building REST APIs with go lang
+
+Things I learnt:
+Go basic syntaxes
+Difference between Arrays and Slices
+File execution
+Managing dependencies
+Use the command 'go get .' to install all dependencies
+
+Questions:
+Use of go.mod file?
+I'm using 'package main' at the start of my file, is it creating a namespace or fetching packages from a certain location?
--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
+// Defining a struct for albums - json tag is used so that when the struct is converted to json the field names are as mentioned in the double quotes
+type album struct {
+	ID     int     `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
 
-	newVar := "this is a new variable"
-	fmt.Println("print value -", newVar)
+// Creating data for the struct
+var albums = []album{
+	{ID: 1, Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: 2, Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: 3, Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+
+	router.Run("localhost:8080")
 }
